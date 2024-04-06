fdf765c6b9e6e57ba34bff6e946bb26f9cc1fb95
Pratik-sys <pithore.pratik@gmail.com>
2024-04-06 20:20:52 +0530

    add engine properties

--- src/Engine.java
+++ src/Engine.java
@@ -1,3 +1,9 @@
 public class Engine {
-    
+
+    private String engineType;
+    private  String fuelType;
+    private String emissionType;
+    private  String capacity;
+    private  int cylinders;
+
 }
